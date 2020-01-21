67199d3d6a2603034e2a58f70524b44882f57612
matheusloures <matheusloures@gmail.com>
2020-01-20 16:33:15 -0800

    1.1.0

--- index.js
+++ index.js
@@ -1,79 +1,72 @@
-#!/usr/bin/env node
 const readline = require('readline');
-const clear = require('clear');
-const figlet = require('figlet');
 const fs = require('fs');
 const path = require('path');
-
-clear();
-
-console.log(
-    figlet.textSync('commandwall', { horizontalLayout: 'full' })
-);
-
+console.log('Leave this window open to serve as support.\r\nAdd your commands below.\r\nEnter saves')
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  prompt: 'cwi> '
 });
 
-rl.on('line', (input) => {
-    // console.log(`Received: ${input}`);
-    var mAnswer = input;
-    if(answer.length!==0){
-        // var containsInit = answer.indexOf('init');
-        // var containsAdd = answer.indexOf('add')
-        // var position = answer[]
-        // if(answer==='add')
-      
-        let b = process.cwd();
-
-        if (!fs.existsSync(path.join(b,'/command_wall.json'))) {
-          fs.writeFile(path.join(b,'/command_wall.json'), [], function(err) {
-            if(err) {
-                console.log("Failed Error #1", err);
-            }
-            console.log('...')
-            }); 
-
-            setTimeout(function(){
-                console.log('...')
-                let a = [];
-                a.push(mAnswer);
-                      
-                        write(JSON.stringify(a),path.join(b, '/command_wall.json')).then(res=>{
+rl.prompt();
+rl.on('line', (line) => {
+    let a = [];
+    let b = process.cwd();
+  switch (line.trim()) {
+    case 'init':
+    if (!fs.existsSync(path.join(b,'/command_wall.json'))) {
+      write(JSON.stringify(a),path.join(b, '/command_wall.json')).then(res=>{
             
-                            console.log("Success!");
-                        },err=>{
-                            console.log("Failed Error #2");
-                        })
-
-                    },3000)
-                      
-        }else{
-            console.log('...')
-
-                let b = process.cwd();
-                var p = path.join(b, '/command_wall.json');
-                var newArr = require(p)
-
-                console.log('...')
-
-                newArr.push(mAnswer)
-
-                        write(JSON.stringify(newArr),path.join(b, '/command_wall.json')).then(res=>{
-            
-                            console.log("Success");
-                        },err=>{
-                            console.log("Failed Error #3");
-                        })
-
-        }
-        
-      rl.close();
+        console.warn("cwi initialized at: "+path.join(b, '/command_wall.json'));
+        },err=>{
+            console.log("Failed Error #2");
+        })
     }else{
-      console.log("Closing...");
-      rl.close();
+        console.log('you already have cwi in your project.')
+        setTimeout(function(){
+            console.log('Run add to start saving commands')
+        },300)
     }
+    break;
+    case 'add':
+    rl.question('write your command and hit enter\r\n', (answer) => {
+        var mAnswer = answer;
+
+            if (!fs.existsSync(path.join(b,'/command_wall.json'))) {
+                fs.writeFile(path.join(b,'/command_wall.json'), [], function(err) {
+                if(err) {
+                    console.warn("cwi not initialized, run init first.", err);
+                }
+                }); 
+                            
+            }else{
+
+                    var p = path.join(b, '/command_wall.json');
+                    var newArr = require(p)
+    
+                    newArr.push(mAnswer)
+    
+                            write(JSON.stringify(newArr),path.join(b, '/command_wall.json')).then(res=>{
+                
+                                console.log("...Success...");
+                                console.log("Run 'add' again to add more");
+                            },err=>{
+                                console.log("Failed Error #3");
+                            })
+    
+            }
+
+        })
+
+
+    break;
+    default:
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Over.');
+  process.exit(0);
 });
 
 function write(what,where){
@@ -87,14 +80,3 @@ function write(what,where){
         }); 
     })
 }
-
-function processFile() {
-    console.log(content);
-}
-  
-
-rl.question('', (answer) => {
-    var mAnswer = answer;
-    console.log('cwi your_command')
-    // console.log('cwi init || cwi add command_text')
-})
